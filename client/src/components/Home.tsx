a341105fd728451f5b5940731276cee7d37ee95b
kanugurajesh <kanugurajesh3@gmail.com>
2024-07-25 22:54:50 +0530

    Added sign in page

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-  
+
   const navigate = useNavigate();
   const [userName, setUserName] = useState<string>("");
 
@@ -14,18 +14,19 @@ const Home = () => {
 
   return (
     <main className="flex flex-col justify-center items-center h-screen">
-      <form action="" onSubmit={handleSubmit} className="w-56 border-2 border-black shadow-2xl rounded-md p-4 flex flex-col gap-4">
-        <h2 className="font-semibold text-black text-center mb-4 text-xl">Sign in to chat</h2>
+      <form action="" onSubmit={handleSubmit} className="border-2 border-black shadow-2xl rounded-xl p-14 flex flex-col gap-4">
+        <h2 className="font-bold text-black text-center mb-6 text-2xl">Sign in to chat</h2>
         <label htmlFor="" className="font-semibold">Username</label>
         <input
           type="text"
           minLength={6}
           name="username"
-          className="border-2 border-black rounded-sm p-1 mb-2"
+          placeholder="Enter your name"
+          className="border-2 border-[#d2d2d2] rounded-md p-2 mb-2 w-full"
           value={userName}
           onChange={(e) => setUserName(e.target.value)}
         />
-        <button className="w-full bg-black text-white p-2 font-bold rounded-md border-2 border-black hover:bg-white hover:text-black transition-all ease-in-out duration-300">Sign In</button>
+        <button className="w-full bg-blue-700 text-white p-2 font-bold rounded-md border-2 border-blue-700 hover:bg-white hover:text-blue-700 transition-all ease-in-out duration-300">Sign In</button>
       </form>
     </main>
   );
