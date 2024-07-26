ed3091849aff2d87c23b416bd36c17fa7ad477cb
kanugurajesh <kanugurajesh3@gmail.com>
2024-07-26 23:01:34 +0530

    Adding docker files

--- client/src/components/chat/ChatBody.tsx
+++ client/src/components/chat/ChatBody.tsx
@@ -1,8 +1,11 @@
 import { useNavigate } from "react-router-dom";
 
 const ChatBody = () => {
+
+  // intializing the navigate
   const navigate = useNavigate();
 
+  // The below function is used to remove the username from the localstorage and navigate the user to the home page
   const handleLeaveChat = () => {
     localStorage.removeItem("userName");
     navigate("/");
@@ -12,7 +15,7 @@ const ChatBody = () => {
   return (
     <div className="flex flex-col">
       <header className="flex items-center gap-4">
-        <p className="font-semibold text-xl">Handout with Colleagues</p>
+        <p className="font-semibold text-xl">Interact with friends</p>
         <button
           className="p-4 bg-red-700 text-white font-bold rounded-md hover:bg-white hover:text-red-700 border-2 border-red-700 transition-all ease-in-out duration-300"
           onClick={handleLeaveChat}
